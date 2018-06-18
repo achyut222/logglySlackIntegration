5d7a0f7b3efeda19c949e39683f42d138fe3a36e
crouchcd <cameroncrouch36@gmail.com>
2018-06-18 12:01:57 -0500

    adding semicolon

--- index.js
+++ index.js
@@ -6,33 +6,33 @@ slack.setWebhook(webhookUri);
 
 // var geoip = require('geoip-lite');
 
-// var ip = "156.40.216.3";
+// var ip = "156.190.216.3";
 // var geo = geoip.lookup(ip);
 
 // console.log(geo);
 
 exports.handler = (input, context, callback) => {
     // console.log('IN FROM GATEWAY:', input)
-    var response = {}
-    var requestType = input.requestContext && input.requestContext.httpMethod
+    var response = {};
+    var requestType = input.requestContext && input.requestContext.httpMethod;
     
     if (!requestType || requestType === 'GET') {
-        response = getGenericResponse()
+        response = getGenericResponse();
     }
     else {
         // parse the body that comes in as a string
-        var logglyJson = JSON.parse(input.body || '{}')
-        var filteredJson = filterLogglyJson(logglyJson)
-        var slackMessage = formatSlackMessage(filteredJson)
-        postToSlack(slackMessage)
-        response = formatGatewayResponse(200, {}, {text: slackMessage}, false)
+        var logglyJson = JSON.parse(input.body || '{}');
+        var filteredJson = filterLogglyJson(logglyJson);
+        var slackMessage = formatSlackMessage(filteredJson);
+        postToSlack(slackMessage);
+        response = formatGatewayResponse(200, {}, {text: slackMessage}, false);
     }
     
     callback(null, response);
 };
 
 function getGenericResponse() {
-    return formatGatewayResponse(200, {"my_header": "header_value"}, {"message": "hello from lambda"}, false)
+    return formatGatewayResponse(200, {"my_header": "header_value"}, {"message": "hello from lambda"}, false);
 }
 
 function formatGatewayResponse(statusCode, headers, body, isBase64Encoded) {
@@ -41,29 +41,29 @@ function formatGatewayResponse(statusCode, headers, body, isBase64Encoded) {
         "headers": headers,
         "body": JSON.stringify(body),
         "isBase64Encoded": isBase64Encoded
-    }
+    };
 }
 
 function filterLogglyJson(logglyJson) {
-    logglyJson = (logglyJson.recent_hits && logglyJson.recent_hits.length) ? logglyJson.recent_hits[0] : '{}'
-    logglyJson = JSON.parse(logglyJson)
+    logglyJson = (logglyJson.recent_hits && logglyJson.recent_hits.length) ? logglyJson.recent_hits[0] : '{}';
+    logglyJson = JSON.parse(logglyJson);
     return {
         errorMessage: logglyJson.errorMessage,
         sourceIPAddress: logglyJson.sourceIPAddress,
         eventSource: logglyJson.eventSource,
         userName: logglyJson.userIdentity && logglyJson.userIdentity.userName,
         eventTime: logglyJson.eventTime,
-    }
+    };
 }
 
 function formatSlackMessage(body) {
-    var errorMessage = body.errorMessage || "{Error Message}"
-    var eventSource = body.eventSource || "{Event Source}"
-    var userName = body.userName || "{User Name}"
-    var sourceIPAddress = body.sourceIPAddress || "{IP Address}"
-    var eventTime = body.eventTime || "{Event Time}"
+    var errorMessage = body.errorMessage || "{Error Message}";
+    var eventSource = body.eventSource || "{Event Source}";
+    var userName = body.userName || "{User Name}";
+    var sourceIPAddress = body.sourceIPAddress || "{IP Address}";
+    var eventTime = body.eventTime || "{Event Time}";
     
-    return `${errorMessage} on ${eventSource} by: ${userName} with IP Address ${sourceIPAddress} at ${eventTime}`
+    return `${errorMessage} on ${eventSource} by: ${userName} with IP Address ${sourceIPAddress} at ${eventTime}`;
 }
 
 function postToSlack(message) {
@@ -76,6 +76,6 @@ function postToSlack(message) {
         });
     }
     catch (e) {
-        console.log('SLACK FAILED')
+        console.log('SLACK FAILED');
     }
 }
